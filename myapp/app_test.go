c590488604ba5761c3b7c99ece5a34f084d1ff75
swucs <swucs7@gmail.com>
2022-03-08 10:54:54 +0900

    First commit!

---
+++ myapp/app_test.go
@@ -0,0 +1,54 @@
+package myapp
+
+import (
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestIndexPathHandler(t *testing.T) {
+
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("Hello world", string(data))
+}
+
+func TestBarPathHandler_WithoutName(t *testing.T) {
+
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("Hello World!", string(data))
+}
+
+func TestBarPathHandler_WithName(t *testing.T) {
+
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar?name=Hannah", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("Hello Hannah!", string(data))
+}
