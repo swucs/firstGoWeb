755aa8b4917a8c615f86fe94ba5af889d0c7fe98
swucs <swucs7@gmail.com>
2022-03-08 13:59:43 +0900

    fileUpload

---
+++ web4/main_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestUpload(t *testing.T) {
+	assert := assert.New(t)
+	path := "D:\\OneDrive - Obigo Inc\\문서\\fms\\FW FMS 공수 자료.msg"
+	file, _ := os.Open(path)
+	defer file.Close()
+
+	os.RemoveAll("./uploads")
+
+	buf := new(bytes.Buffer)
+	writer := multipart.NewWriter(buf)
+	multi, err := writer.CreateFormFile("upload_file", filepath.Base(path))
+	assert.NoError(err)
+	io.Copy(multi, file)
+	writer.Close()
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("POST", "/uploads", buf)
+	req.Header.Set("Content-type", writer.FormDataContentType())
+
+	uploadsHandler(res, req)
+	assert.Equal(http.StatusOK, res.Code)
+
+	uploadFilePath := "./uploads/" + filepath.Base(path)
+	_, err = os.Stat(uploadFilePath)
+	assert.NoError(err)
+
+	uploadFile, _ := os.Open(uploadFilePath)
+	originFile, _ := os.Open(path)
+	defer uploadFile.Close()
+	defer originFile.Close()
+
+	uploadData := []byte{}
+	originData := []byte{}
+	uploadFile.Read(uploadData)
+	originFile.Read(originData)
+
+	assert.Equal(originData, uploadData)
+}
