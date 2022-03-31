3ed321902328076694ccb0b698e10a65f79f2e73
swucs <swucs7@gmail.com>
2022-03-31 17:23:36 +0900

    feat: Error handling 2
    GET Customer Application Error 객체 생성 & 500 에러처리

--- banking/app/handlers.go
+++ banking/app/handlers.go
@@ -39,10 +39,10 @@ func (ch *CustomerHandlers) getAllCustomers(w http.ResponseWriter, r *http.Reque
 func (ch *CustomerHandlers) getCustomer(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["customer_id"]
-	customer, err := ch.service.GetCustomer(id)
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(w, err.Error())
+	customer, appErr := ch.service.GetCustomer(id)
+	if appErr != nil {
+		w.WriteHeader(appErr.Code)
+		fmt.Fprintf(w, appErr.Message)
 	} else {
 		w.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(customer)
